ad31aae41d8b144ba110991cad3be215bb53ba66
jogoscarlos96-del <jogoscarlos96@gmail.com>
2026-09-14 11:20:41 +0100

    Export Mega Evolution store

--- src/lib/pokemon/mega/index.ts
+++ src/lib/pokemon/mega/index.ts
@@ -1,3 +1,4 @@
 export * from "./MegaEvolution"
+export * from "./MegaEvolutionStore"
 export { default as MegaEvolutionControl } from "./MegaEvolutionControl.svelte"
 export { default as MegaFormsFieldset } from "./MegaFormsFieldset.svelte"
