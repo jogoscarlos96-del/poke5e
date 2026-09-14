d2cc91123b943b762104efca88eead1a5b4e398c
jogoscarlos96-del <jogoscarlos96@gmail.com>
2026-09-14 11:18:12 +0100

    Export Mega Evolution helpers

---
+++ src/lib/pokemon/mega/index.ts
@@ -0,0 +1,3 @@
+export * from "./MegaEvolution"
+export { default as MegaEvolutionControl } from "./MegaEvolutionControl.svelte"
+export { default as MegaFormsFieldset } from "./MegaFormsFieldset.svelte"
