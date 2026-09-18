0991b79fb6b51f11d8867aa0592710147a77502a
jogoscarlos96-del <jogoscarlos96@gmail.com>
2026-09-18 17:50:50 +0100

    Remove obsolete dynamic damage reset workaround

--- src/lib/moves/learned/BaseMoveDamageRoll.svelte
+++ src/lib/moves/learned/BaseMoveDamageRoll.svelte
@@ -22,7 +22,6 @@
 	let criticalBonus: number | undefined = undefined
 	let total: number | undefined = undefined
 	let error: string | undefined = undefined
-	let previousDamageDice = damage.dice
 
 	$: label = damage.isHealing ? "Healing" : "Damage"
 	$: canApplyHealing = damage.isHealing && onapplyhealing != null
@@ -44,13 +43,6 @@
 		criticalBonus = undefined
 		total = undefined
 		error = undefined
-	}
-
-	$: if (damage.dice !== previousDamageDice) {
-		previousDamageDice = damage.dice
-		transientDamageBonus = 0
-		extraDice = ""
-		clearRoll()
 	}
 
 	const changeTransientDamageBonus = (amount: number) => {
